17df996d06fcaa2328d2266e0c92619adf204fb9
Papiricoh <slendeman0200@gmail.com>
2023-06-11 05:34:52 +0200

    added Nation.java

--- src/main/java/org/papiricoh/civilizationplugin/nation/City.java
+++ src/main/java/org/papiricoh/civilizationplugin/nation/City.java
@@ -2,7 +2,6 @@ package org.papiricoh.civilizationplugin.nation;
 
 import org.papiricoh.civilizationplugin.nation.range.CityType;
 import org.papiricoh.civilizationplugin.player.CityPlayer;
-import org.papiricoh.civilizationplugin.player.Player;
 import org.papiricoh.civilizationplugin.player.roles.CityRole;
 
 import java.util.ArrayList;
@@ -30,6 +29,10 @@ public class City {
         this.members.add(player);
     }
 
+    public ArrayList<CityPlayer> getMembers() {
+        return this.members;
+    }
+
     public void setPreName(String pre_name) {
         this.pre_name = pre_name;
     }
@@ -42,12 +45,30 @@ public class City {
         return this.pre_name + " " + this.name;
     }
 
+    public CityPlayer getPlayerByName(String name) {
+        return findPlayerByName(name);
+    }
+
     public boolean changePlayerRole(String user_player, String target_player, int new_role) {
         CityPlayer user_player_obj = findPlayerByName(user_player);
-        if(user_player_obj != null) {
+        if(user_player.equals(target_player) && user_player_obj != null && this.members.size() > 1) {
+            user_player_obj.setRole(CityRole.values()[new_role]);
+            int max_role = 0;
+            CityPlayer max_player = null;
+            for (int i = 0; i < this.members.size(); i++) {
+                if( max_role < this.members.get(i).getRole().ordinal()) {
+                    max_role = this.members.get(i).getRole().ordinal();
+                    max_player = this.members.get(i);
+                }
+            }
+            max_player.setRole(CityRole.LEADER);
+            return true;
+
+        }else if(user_player_obj != null && user_player_obj.getRole() == CityRole.LEADER) {
             CityPlayer target_player_obj = findPlayerByName(target_player);
             if(target_player_obj != null) {
                 target_player_obj.setRole(CityRole.values()[new_role]);
+                return true;
             }
             return false;
         }
--- test/java/CityPlayerTest.java
+++ test/java/CityPlayerTest.java
@@ -1,11 +1,54 @@
 import org.junit.Test;
+import org.papiricoh.civilizationplugin.nation.City;
+import org.papiricoh.civilizationplugin.nation.range.CityType;
+import org.papiricoh.civilizationplugin.player.CityPlayer;
+import org.papiricoh.civilizationplugin.player.roles.CityRole;
+
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 
-class CityPlayerTest {
+public class CityPlayerTest {
 
     @Test
-    void firstTest() {
-        assertEquals(1, 1);
+    public void CityLeaderChangesRoleToMemberTest() {
+        CityPlayer cp1 = new CityPlayer(UUID.randomUUID(), "Papiricoh", CityRole.LEADER.ordinal());
+        CityPlayer cp2 = new CityPlayer(UUID.randomUUID(), "Dark Annata", CityRole.HIGH_OFFICIAL.ordinal());
+        CityPlayer cp3 = new CityPlayer(UUID.randomUUID(), "Lith Lotia", CityRole.CITIZEN.ordinal());
+        City tCity = new City("Lothalia", CityType.SETTLEMENT);
+        tCity.addPlayer(cp1);
+        tCity.addPlayer(cp2);
+        tCity.addPlayer(cp3);
+        tCity.changePlayerRole(cp1.getName(), cp2.getName(), CityRole.OFFICIAL.ordinal());
+
+        assertEquals(CityRole.OFFICIAL, tCity.getPlayerByName(cp2.getName()).getRole());
+    }
+
+    @Test
+    public void CityNotLeaderChangesRoleToMemberTest() {
+        CityPlayer cp1 = new CityPlayer(UUID.randomUUID(), "Papiricoh", CityRole.LEADER.ordinal());
+        CityPlayer cp2 = new CityPlayer(UUID.randomUUID(), "Dark Annata", CityRole.HIGH_OFFICIAL.ordinal());
+        CityPlayer cp3 = new CityPlayer(UUID.randomUUID(), "Lith Lotia", CityRole.CITIZEN.ordinal());
+        City tCity = new City("Lothalia", CityType.SETTLEMENT);
+        tCity.addPlayer(cp1);
+        tCity.addPlayer(cp2);
+        tCity.addPlayer(cp3);
+        tCity.changePlayerRole(cp3.getName(), cp2.getName(), CityRole.OFFICIAL.ordinal());
+
+        assertEquals(CityRole.HIGH_OFFICIAL, tCity.getPlayerByName(cp2.getName()).getRole());
+    }
+
+    @Test
+    public void CityLeaderChangesRoleToHimselfTest() {
+        CityPlayer cp1 = new CityPlayer(UUID.randomUUID(), "Papiricoh", CityRole.LEADER.ordinal());
+        CityPlayer cp2 = new CityPlayer(UUID.randomUUID(), "Dark Annata", CityRole.HIGH_OFFICIAL.ordinal());
+        CityPlayer cp3 = new CityPlayer(UUID.randomUUID(), "Lith Lotia", CityRole.CITIZEN.ordinal());
+        City tCity = new City("Lothalia", CityType.SETTLEMENT);
+        tCity.addPlayer(cp1);
+        tCity.addPlayer(cp2);
+        tCity.addPlayer(cp3);
+        tCity.changePlayerRole(cp1.getName(), cp1.getName(), CityRole.OFFICIAL.ordinal());
+
+        assertEquals(CityRole.LEADER, tCity.getPlayerByName(cp2.getName()).getRole());
     }
 }
