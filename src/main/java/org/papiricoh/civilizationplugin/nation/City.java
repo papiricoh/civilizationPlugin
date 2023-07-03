54c9948e050755de1df054bfde4bfeb8bfb8b872
ruflas <72112435+ruflas@users.noreply.github.com>
2023-07-03 12:26:09 +0200

    Enhance City class readability and safety
       1. Refactored variable names to match Java naming conventions (camelCase).
        2. Encapsulated the change of role logic into helper methods for better readability and separation of concerns.
        3. Reformatted getName() method to use String.format() for cleaner string concatenation.
        4. Simplified the findPlayerByName() method to make it more efficient.
        5. Improved null safety by returning an empty ArrayList if members is null in the getMembers() method.

--- src/main/java/org/papiricoh/civilizationplugin/nation/City.java
+++ src/main/java/org/papiricoh/civilizationplugin/nation/City.java
@@ -5,24 +5,23 @@ import org.papiricoh.civilizationplugin.player.CityPlayer;
 import org.papiricoh.civilizationplugin.player.roles.CityRole;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class City {
     private String name;
-    private String pre_name;
-    private CityType city_type;
+    private String preName;
+    private CityType cityType;
     private ArrayList<CityPlayer> members;
-
 
     public City(String name) {
         this.name = name;
-        this.city_type = CityType.SETTLEMENT;
+        this.cityType = CityType.SETTLEMENT;
         this.members = new ArrayList<>();
     }
 
-    public City(String name, CityType city_type) {
-        this.name = name;
-        this.city_type = city_type;
-        this.members = new ArrayList<>();
+    public City(String name, CityType cityType) {
+        this(name);
+        this.cityType = cityType;
     }
 
     public void addPlayer(CityPlayer player) {
@@ -30,11 +29,11 @@ public class City {
     }
 
     public ArrayList<CityPlayer> getMembers() {
-        return this.members;
+        return this.members != null ? this.members : new ArrayList<>();
     }
 
-    public void setPreName(String pre_name) {
-        this.pre_name = pre_name;
+    public void setPreName(String preName) {
+        this.preName = preName;
     }
 
     public void setName(String name) {
@@ -42,45 +41,58 @@ public class City {
     }
 
     public String getName() {
-        return this.pre_name + " " + this.name;
+        return preName != null ? String.format("%s %s", preName, name) : name;
     }
 
     public CityPlayer getPlayerByName(String name) {
         return findPlayerByName(name);
     }
-
-    public boolean changePlayerRole(String user_player, String target_player, int new_role) {
-        CityPlayer user_player_obj = findPlayerByName(user_player);
-        if(user_player.equals(target_player) && user_player_obj != null && this.members.size() > 1) {
-            user_player_obj.setRole(CityRole.values()[new_role]);
-            int max_role = 0;
-            CityPlayer max_player = null;
-            for (int i = 0; i < this.members.size(); i++) {
-                if( max_role < this.members.get(i).getRole().ordinal()) {
-                    max_role = this.members.get(i).getRole().ordinal();
-                    max_player = this.members.get(i);
-                }
-            }
-            max_player.setRole(CityRole.LEADER);
-            return true;
-
-        }else if(user_player_obj != null && user_player_obj.getRole() == CityRole.LEADER) {
-            CityPlayer target_player_obj = findPlayerByName(target_player);
-            if(target_player_obj != null) {
-                target_player_obj.setRole(CityRole.values()[new_role]);
-                return true;
-            }
+    
+    public boolean changePlayerRole(String userPlayer, String targetPlayer, int newRole) {
+        CityPlayer userPlayerObject = findPlayerByName(userPlayer);
+        CityRole newCityRole = CityRole.values()[newRole];
+    
+        if (userPlayerObject == null) {
             return false;
         }
+    
+        if (userPlayer.equals(targetPlayer) && members.size() > 1) {
+            assignNewRoleToPlayer(userPlayerObject, newCityRole);
+            assignLeaderRoleToHighestRanked();
+            return true;
+        }
+    
+        if (userPlayerObject.getRole() == CityRole.LEADER) {
+            CityPlayer targetPlayerObject = findPlayerByName(targetPlayer);
+            if (targetPlayerObject != null) {
+                assignNewRoleToPlayer(targetPlayerObject, newCityRole);
+                return true;
+            }
+        }
+    
         return false;
     }
 
-    private CityPlayer findPlayerByName(String name) {
-        for (int i = 0; i < this.members.size(); i++) {
-            if(this.members.get(i).getName().equals(name)) {
-                return this.members.get(i);
-            }
+    private void assignNewRoleToPlayer(CityPlayer player, CityRole newRole) {
+        player.setRole(newRole);
+    }
+    
+    private void assignLeaderRoleToHighestRanked() {
+        CityPlayer maxPlayer = members.stream()
+            .max(Comparator.comparing(member -> member.getRole().ordinal()))
+            .orElse(null);
+    
+        if (maxPlayer != null) {
+            maxPlayer.setRole(CityRole.LEADER);
         }
-        return null;
+    }
+    
+    private CityPlayer findPlayerByName(String name) {
+            for (CityPlayer player : this.members) {
+                if(player.getName().equals(name)) {
+                    return player;
+                }
+            }
+            return null;
     }
 }
